6b7d804f0506acbcb5c9238c508629c0dcf36241
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-25 15:43:44 +0200

    1/9 - Refactorisation en fonctions

---
+++ libraries/database.php
@@ -0,0 +1,10 @@
+<?php
+
+function getPdo(){
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', 'Humans31!', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    return $pdo;
+}
